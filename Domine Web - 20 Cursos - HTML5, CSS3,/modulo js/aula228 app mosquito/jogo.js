3e65bf5b05072ab98d7f2455757e92ee46e0761f
Lucas <lcsntc1@gmail.com>
2024-07-01 11:37:58 -0300

    att game mosquito

--- Domine Web - 20 Cursos - HTML5, CSS3,/modulo js/aula228 app mosquito/jogo.js
+++ Domine Web - 20 Cursos - HTML5, CSS3,/modulo js/aula228 app mosquito/jogo.js
@@ -9,4 +9,19 @@ function ajustaTamanhoTela()
 }
 
 ajustaTamanhoTela()
-console.log(altura,largura)
+//console.log(altura,largura)
+
+function posicaoRandomica()
+{
+    var posicaoX = Math.floor(Math.random() * largura)
+    var posicaoY = Math.floor(Math.random() * altura)
+
+    console.log(posicaoX, posicaoY)
+
+    //criando elemento html
+
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosca.png'
+    mosquito.className = `mosquito1`
+    document.body.appendChild(mosquito)
+}
