a1395eee5aa28209f17a1f61f080e10004d1ef74
Lucas <lcsntc1@gmail.com>
2024-07-01 10:08:09 -0300

    att game mata mosquito

---
+++ Domine Web - 20 Cursos - HTML5, CSS3,/modulo js/aula228 app mosquito/jogo.js
@@ -0,0 +1,12 @@
+var altura = 0
+var largura = 0
+
+function ajustaTamanhoTela()
+{
+    altura = window.innerHeight
+    largura = window.innerWidth
+    console.log(altura,largura)
+}
+
+ajustaTamanhoTela()
+console.log(altura,largura)
