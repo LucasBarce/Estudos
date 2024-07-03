eaf51a2fabe4021ecf7c53997e8ea90899eb4d69
Lucas <lcsntc1@gmail.com>
2024-07-03 19:10:52 -0300

    att

--- Domine Web - 20 Cursos - HTML5, CSS3,/modulo js/aula228 app mosquito/jogo.js
+++ Domine Web - 20 Cursos - HTML5, CSS3,/modulo js/aula228 app mosquito/jogo.js
@@ -1,5 +1,6 @@
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoTela()
 {
@@ -17,6 +18,14 @@ function posicaoRandomica()
     if(document.getElementById('mosquito'))
         {
             document.getElementById('mosquito').remove()
+            if(vidas > 3)
+            {
+                window.location.href = `fim_de_jogo_html`
+            }else
+            {
+                document.getElementById(`v` + vidas).src = 'imagens/coracao_vazio.png'
+                vidas++
+            }
         }
 
     var posicaoX = Math.floor(Math.random() * largura) - 90
@@ -36,6 +45,7 @@ function posicaoRandomica()
     mosquito.style.top = posicaoY + `px`
     mosquito.style.position = `absolute`
     mosquito.id = `mosquito`
+    mosquito.onclick = function(){this.remove()}
 
     document.body.appendChild(mosquito)
 
